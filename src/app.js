35f8ff9cc39b7204fd0edf9e867abd7f6d00c794
JohnD42 <139003527+JohnD42@users.noreply.github.com>
2023-08-03 00:55:27 +0000

    Coded behavior described in project description.

--- src/app.js
+++ src/app.js
@@ -2,10 +2,38 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload;
+{
+  function onload() {
+    let who = ["The dog", "My grandma", "His turtle", "My bird"];
+    let action = [" ate", " peed on", " crushed", " broke"];
+    let what = [" my homework", " the keys", " the car"];
+    let when = [
+      " before the class.",
+      " right on time.",
+      " when I finished.",
+      " during my lunch.",
+      " while I was praying."
+    ];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+    function randoGen() {
+      let randoNum = Math.random();
+      let randoNum2 = randoNum * 4;
+      let randoNum3 = Math.floor(randoNum2);
+      return randoNum3;
+    }
+
+    let excuse = [];
+
+    excuse.push(who[randoGen()]);
+    excuse.push(action[randoGen()]);
+    excuse.push(what[randoGen()]);
+    excuse.push(when[randoGen()]);
+
+    let excuseString = excuse.join("");
+
+    document.getElementById("excuse").innerHTML = excuseString;
+  }
+  onload();
+  clickToGenerateExcuse.addEventListener("click", onload);
+}
