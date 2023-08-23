f525c457009ef13631b09275771d0dc95d140016
JohnD42 <139003527+JohnD42@users.noreply.github.com>
2023-08-23 03:05:58 +0000

    Updated

--- src/app.js
+++ src/app.js
@@ -6,14 +6,14 @@ window.onload;
 {
   function onload() {
     let who = ["The dog", "My grandma", "His turtle", "My bird"];
-    let action = [" ate", " peed on", " crushed", " broke"];
-    let what = [" my homework", " the keys", " the car", " my laptop"];
+    let action = ["ate", "peed on", "crushed", "broke"];
+    let what = ["my homework", "the keys", "the car", "my laptop"];
     let when = [
-      " before the class.",
-      " right on time.",
-      " when I finished.",
-      " during my lunch.",
-      " while I was praying."
+      "before the class.",
+      "right on time.",
+      "when I finished.",
+      "during my lunch.",
+      "while I was praying."
     ];
 
     const randoGen = maxNum => {
@@ -29,7 +29,7 @@ window.onload;
       when[randoGen(when.length - 1)]
     ];
 
-    let excuseString = excuseArray.join("");
+    let excuseString = excuseArray.join(" ");
 
     document.getElementById("excuse").innerHTML = excuseString;
   }
