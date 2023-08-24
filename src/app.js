591681f555935a942aa7509619e9c052d47749cb
JohnD42 <139003527+JohnD42@users.noreply.github.com>
2023-08-24 02:06:04 +0000

    Updated project to add relevant bg-imgs to excuses

--- src/app.js
+++ src/app.js
@@ -4,16 +4,17 @@ import "./style.css";
 
 window.onload;
 {
+  let currentClass = "dog";
   function onload() {
     let who = ["The dog", "My grandma", "His turtle", "My bird"];
-    let action = ["ate", "peed on", "crushed", "broke"];
-    let what = ["my homework", "the keys", "the car", "my laptop"];
+    let action = ["ate", "peed", "crushed", "broke"];
+    let what = ["my homework", "the keys", "the car"];
     let when = [
-      "before the class.",
-      "right on time.",
-      "when I finished.",
-      "during my lunch.",
-      "while I was praying."
+      "before the class",
+      "right on time",
+      "when I finished",
+      "during my lunch",
+      "while I was praying"
     ];
 
     const randoGen = upperBound => {
@@ -32,7 +33,33 @@ window.onload;
     let excuseString = excuseArray.join(" ");
 
     document.getElementById("excuse").innerHTML = excuseString;
+    if (excuseString.includes("dog")) {
+      document.getElementById("bg-img").classList.remove(currentClass);
+      document.getElementById("bg-img").classList.add("dog");
+      currentClass = "dog";
+      document.getElementById("excuse").innerHTML = excuseString;
+    }
+
+    if (excuseString.includes("bird")) {
+      document.getElementById("bg-img").classList.remove(currentClass);
+      document.getElementById("bg-img").classList.add("bird");
+      currentClass = "bird";
+      document.getElementById("excuse").innerHTML = excuseString;
+    }
+
+    if (excuseString.includes("turtle")) {
+      document.getElementById("bg-img").classList.remove(currentClass);
+      document.getElementById("bg-img").classList.add("turtle");
+      currentClass = "turtle";
+      document.getElementById("excuse").innerHTML = excuseString;
+    }
+
+    if (excuseString.includes("grandma")) {
+      document.getElementById("bg-img").classList.remove(currentClass);
+      document.getElementById("bg-img").classList.add("grandma");
+      currentClass = "grandma";
+      document.getElementById("excuse").innerHTML = excuseString;
+    }
   }
-  onload();
   clickToGenerateExcuse.addEventListener("click", onload);
 }
